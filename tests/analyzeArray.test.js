b6d7c240e0ed52015d374a7ec37570f238df3dd9
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2023-12-23 11:52:18 +0100

    Add array with negative numbers test

--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -5,6 +5,10 @@ test("should return correct values for a valid integer array", () => {
   expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({ average: 4, min: 1, max: 8, length: 6 });
 });
 
+test("should return correct values for an array with negative numbers", () => {
+  expect(analyzeArray([-5, -2, 0, 3, 7])).toEqual({ average: 0.6, min: -5, max: 7, length: 5 });
+});
+
 test("should return correct values for a valid decimal array", () => {
   expect(analyzeArray([1.2, 4, 3.443])).toEqual({ average: 2.88, min: 1.2, max: 4, length: 3 });
 });
