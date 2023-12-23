0a1abdc5f2584139a98bf920a1f23eda1863a7f0
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2023-12-23 11:48:52 +0100

    Improve test descriptions

--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -1,23 +1,23 @@
 // analyzeArray.test.js
 const analyzeArray = require("../utils/analyzeArray");
 
-test("should return the correct values on an integer array", () => {
+test("should return correct values for a valid integer array", () => {
   expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({ average: 4, min: 1, max: 8, length: 6 });
 });
 
-test("should return the correct values on a decimal array", () => {
+test("should return correct values for a valid decimal array", () => {
   expect(analyzeArray([1.2, 4, 3.443])).toEqual({ average: 2.88, min: 1.2, max: 4, length: 3 });
 });
 
-test("should return error message on non-array inputs", () => {
+test("should handle a non-array input", () => {
   expect(analyzeArray("hello")).toBe("Invalid input. Please provide a non-empty array of numbers.");
 });
 
-test("should return error message on empty array inputs", () => {
+test("should handle an empty array", () => {
   expect(analyzeArray([])).toBe("Invalid input. Please provide a non-empty array of numbers.");
 });
 
-test("should return error message on non-numerical array inputs", () => {
+test("should handle an array with non-number elements", () => {
   expect(analyzeArray([1, 2, "three"])).toBe(
     "Invalid input. Please provide a non-empty array of numbers."
   );
