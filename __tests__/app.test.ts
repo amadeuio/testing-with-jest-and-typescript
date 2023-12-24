643e6470be54a6f031b16ec64d793d3df8f6f8ec
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2023-12-24 17:18:27 +0100

    Add starting boilerplate files with example test

---
+++ __tests__/app.test.ts
@@ -0,0 +1,6 @@
+// __tests__/app.test.ts
+import add from '../src/app';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+// src/app.ts
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+export default add;
