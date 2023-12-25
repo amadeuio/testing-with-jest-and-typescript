927f6e4d8e1c4f4e6ee20fa538e3df1c25bbbdaa
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2023-12-25 11:06:59 +0100

    Remove console.log from analyzeArray

--- src/analyzeArray.ts
+++ src/analyzeArray.ts
@@ -20,6 +20,6 @@ function analyzeArray(
 }
 
 // Example usage
-console.log(analyzeArray([1, 8, 3, 4, 2, 6])); // { average: 4, min: 1, max: 8, length: 6 }
+analyzeArray([1, 8, 3, 4, 2, 6]); // { average: 4, min: 1, max: 8, length: 6 }
 
 export default analyzeArray;
